5cb3103fd16fd320c0f89f4c6b0845fa0be1353f
Harsh Raj Dubey <harsh7890@gmail.com>
2026-07-05 00:40:32 +0530

    fix: Council 2026-27 Updated

--- src/views/Contacts/RenderContacts.js
+++ src/views/Contacts/RenderContacts.js
@@ -5,7 +5,7 @@ import PreviousOfficeBearers from "../../views/Contacts/PreviousOfficeBearers";
 export default function RenderContactsFromDropdown({ option }) {
   return (
     <>
-      {option === "CURRENT OFFICE BEARERS" && <CurrentOfficeBearers year="2025-2026"/>}
+      {option === "CURRENT OFFICE BEARERS" && <CurrentOfficeBearers year="2026-2027"/>}
       {option === "PREVIOUS OFFICE BEARERS" && <PreviousOfficeBearers />}
       {option === "STAFF" && <Staff />}
     </>
